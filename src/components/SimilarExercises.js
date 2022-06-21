1d8838d8673f131694c415eb91690b76a6a65bc8
abhijeetpalanki <ajpalanki@gmail.com>
2022-06-20 19:29:00 -0500

    finalized rex app

---
+++ src/components/SimilarExercises.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import HorizonalScrollBar from "./HorizonalScrollBar";
+import Loader from "./Loader";
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <Box sx={{ marginTop: { lg: "100px", xs: 0 } }}>
+      <Typography variant="h3" mb={5}>
+        Exercises that target the same muscle group
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {targetMuscleExercises.length ? (
+          <HorizonalScrollBar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+
+      <Typography variant="h3" mb={5}>
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {equipmentExercises.length ? (
+          <HorizonalScrollBar data={equipmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default SimilarExercises;
